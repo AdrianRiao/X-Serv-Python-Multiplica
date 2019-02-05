068659c10c0704fe980785599f0f7d57d4922db0
Adrian Omar Riao Monsalve <aoriao@gamma10.aulas.gsyc.urjc.es>
2019-02-05 12:44:56 +0100

    Se adjunta código tablas de multiplicar

---
+++ tablas.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+for n1 in range(1,11):
+	print("Tabla del " + str(n1))
+	print("-----------")
+	for n2 in range(1,11):
+		print(str(n1) + " por " + str(n2) + " es " + str(n1*n2))
